c78c6950f57fcac9d4a2a03556aead123594189f
Kullanıcı Adınız <e-posta@adresiniz.com>
2023-07-06 16:22:54 +0300

    main
    main

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const logger = require("morgan");
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+const categoryRoute = require("./routes/categories.js");
+const productRoute = require("./routes/products.js");
+const billRoute = require("./routes/bills.js");
+const authRoute = require("./routes/auth.js");
+const userRoute = require("./routes/users.js");
+
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+
+// middlewares
+app.use(logger("dev"));
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/categories", categoryRoute);
+app.use("/api/products", productRoute);
+app.use("/api/bills", billRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(port, () => {
+    connect();
+  console.log(`Example app listening on port ${port}`);
+});
